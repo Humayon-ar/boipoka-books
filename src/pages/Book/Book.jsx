b0258bd1410b2593ae4bf093bc0101dfa949218c
Humayon Kabir <contact.humayon@gmail.com>
2025-04-20 11:50:18 +0600

    Cards have been added and loaded

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,12 +1,37 @@
+// Icons
+import { FaStarHalfAlt } from "react-icons/fa";
 
-const Book = ({book}) => {
+const Book = ({ book }) => {
 
-    const {bookId, bookName, author, image, rating, totalPages, category, tags, yearOfPublishing} = book;
-    console.log(bookName)
+
+
+    const { bookId, bookName, author, image, rating, totalPages, category, tags, yearOfPublishing } = book;
+
     return (
-        <div>
-            This is book
+
+        <div className="card bg-base-100 shadow-sm p-3">
+            <figure className="h-[200px] ">
+                <img className="object-cover rounded" src={image} alt={bookName} />
+            </figure>
+            <div className="p-3 space-y-3">
+                <div className="flex gap-2">
+                    {
+                        tags.map(tag => <span className="text-green-800 font-semibold p-2 bg-slate-100 rounded-lg"> {tag} </span>)
+                    }
+                </div>
+
+                <h2 className="text-xl font-semibold "> {bookName} </h2>
+                <p className="font-semibold text-gray-600">By: {author} </p>
+
+                <div className="flex justify-between font-semibold text-gray-600">
+                    <p> {category} </p>
+                    <p className="flex items-center gap-1"> {rating} <FaStarHalfAlt /> </p>
+                </div>
+
+            </div>
+
         </div>
+
     )
 }
 
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -6,7 +6,7 @@ const Books = ({ data }) => {
     return (
         <div>
             <h1 className="text-center text-3xl font-bold my-5">Books</h1>
-            <div className="books-container">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-10">
                 <Suspense fallback={<span>Loading...</span>}>
                     {
                         data.map(book => <Book key={book.bookId} book={book}></Book>)
